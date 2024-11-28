fa95923f8053f7ab161173d90b3d50ed2fe4e6cf
gabrielepolidori <polidori.gabriele.97@gmail.com>
2024-11-28 09:53:26 -0500

    Add files via upload

---
+++ progettos2l2.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main ()
+{int numero1, numero2, risultato;
+
+printf("calcoliamo la moltiplicazione tra due numeri.\n");
+printf("inserisci il primo numero:");
+scanf("%d", &numero1);
+printf("inserisci il secondo numero:");
+
+scanf("%d", &numero2);
+
+risultato= numero1*numero2;
+
+printf("il risultato dei due numeri è: %d\n",risultato);}
+
+
+
